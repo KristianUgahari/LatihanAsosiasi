d5dd3f300beb602d4ff9cab2481168aa32c4325f
Kristian Ugahari Dwipa <48380624+KristianUgahari@users.noreply.github.com>
2019-07-02 14:57:40 +0700

    Add files via upload

---
+++ LatihanAsosiasi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAsosiasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek kartu
+            Kartu kartu = new Kartu();
+
+            //membuat objek karyawan
+            Karyawan karyawan = new Karyawan();
+            karyawan.Nama = "Manut";
+            karyawan.Status = false; //aktif
+
+            //hubungan asosiasi
+            if (karyawan.Login(kartu))
+            {
+                Console.WriteLine("Data Karyawan Sangat Super Valid Sekali");
+            }
+            else
+            {
+                Console.WriteLine("Maaf Data Karyawan Sangat Super Tidak Valid Sekali");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
